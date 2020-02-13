44094ee1bed3981da2db60d7b4a552e48589cbd4
omer kor <omerkorr@gmail.com>
2020-02-13 09:56:38 +0300

    last

--- YemekMenusu/YemekMenusu/Program.cs
+++ YemekMenusu/YemekMenusu/Program.cs
@@ -10,6 +10,46 @@ namespace YemekMenusu
     {
         static void Main(string[] args)
         {
+            char secim;
+            double toplam = 0;
+            do
+            {
+                Console.WriteLine("1- sandviç - 12.50TL");
+                Console.WriteLine("2- pizza - 14.50TL");
+                Console.WriteLine("3- kebap - 11.50TL");
+                Console.WriteLine("4- çorba - 8.50TL");
+                Console.WriteLine("5- tost - 6.50TL");
+                Console.WriteLine("6- çıkış");
+                Console.WriteLine("Seçiminizi yapın");
+                secim = Convert.ToChar(Console.ReadLine());
+                Console.Clear();
+                switch (secim)
+                {
+                    case '1':
+                        toplam += 12.50;
+                        break;
+                    case '2':
+                        toplam += 14.50;
+                        break;
+                    case '3':
+                        toplam += 11.50;
+                        break;
+                    case '4':
+                        toplam += 8.50;
+                        break;
+                    case '5':
+                        toplam += 6.50;
+                        break;
+                    case '6':
+                        Console.WriteLine("Alışverişiniz tamamlanmıştır.");
+                        break;
+                    default:
+                        Console.WriteLine("1-6 arası seçim yapınız.");
+                        break;
+                }
+            } while (secim != '6');
+            Console.WriteLine("Toplam hesabınız {0} TL",toplam);
+            Console.Read();
         }
     }
 }
